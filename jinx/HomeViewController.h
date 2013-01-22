8779aa7845786727da7f922d6b6e1e410b54dbf0
Andrew Patterson <andrew.patterson@duke.edu>
2013-01-22 17:30:06 -0500

    b

---
+++ jinx/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  jinx
+//
+//  Created by Andrew Patterson on 1/22/13.
+//  Copyright (c) 2013 Duke University. All rights reserved.
+//
+
+#import "GenericViewController.h"
+
+@interface HomeViewController : GenericViewController
+
+@end
