c1a8442f54470990157520d43dc8bcaba23e3308
Alexander Bruce <ajbruce60@gmail.com>
2013-01-30 20:06:52 -0500

    sdssdsds beta 1

--- jinx/StatsViewController.m
+++ jinx/StatsViewController.m
@@ -1,5 +1,7 @@
 #import "StatsViewController.h"
 #import "Constants.h"
+#import <math.h>
+
 @interface StatsViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *longLabel;
 @property (weak, nonatomic) IBOutlet UILabel *shortLabel;
@@ -26,8 +28,11 @@
 {
     [super viewDidLoad];
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
     self.longLabel.text = [NSString stringWithFormat:@"Longest Word: %@ ",[defaults objectForKey:LONGEST_WORD] ];
+    if([defaults objectForKey:LONGEST_WORD]==NULL)self.longLabel.text = [NSString stringWithFormat:@"Longest Word: %@ ",@"N/A" ];
     self.shortLabel.text = [NSString stringWithFormat:@"Shortest Word: %@ ",[defaults objectForKey:SHORTEST_WORD] ];
+    if([defaults objectForKey:SHORTEST_WORD]==NULL) self.shortLabel.text = [NSString stringWithFormat:@"Shortest Word: %@ ",@"N/A" ];
     
     NSNumber* n = (NSNumber*)[defaults objectForKey: TOTAL_ROUNDS];
     int totalRounds = [n intValue];
@@ -35,11 +40,15 @@
     NSNumber* p = (NSNumber*)[defaults objectForKey:TIMES_PLAYED];
     double average = (totalRounds*1.0)/([p intValue]*1.0);
     self.averageLabel.text = [NSString stringWithFormat:@"Average rounds played: %f",average];
+    if(isnan(average))
+    self.averageLabel.text = [NSString stringWithFormat:@"Average rounds played: %@",@"N/A"];
     
     self.slowLabel.text = [NSString stringWithFormat:@"Longest game: %@", [defaults objectForKey: SLOWEST_ROUND]];
+    if([defaults objectForKey: SLOWEST_ROUND] == NULL) self.slowLabel.text = [NSString stringWithFormat:@"Longest game: %@", @"N/A"];
     self.fastLabel.text = [NSString stringWithFormat:@"Shortest game: %@", [defaults objectForKey: FASTEST_ROUND]];
+    if([defaults objectForKey: FASTEST_ROUND] == NULL)self.fastLabel.text = [NSString stringWithFormat:@"Shortest game: %@", @"N/A"];
     NSDictionary* dict = [defaults objectForKey:MOST_POPULAR_DIC];
-    NSString* mostCommon =@"";
+    NSString* mostCommon =@"N/A";
     int max =0;
     for (NSString* cur in [dict allKeys]) {
         NSNumber* num = [dict objectForKey:cur];
